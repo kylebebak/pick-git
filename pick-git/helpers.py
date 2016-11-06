2eb90f7b6e037bc247344d45d48809ec95332023
Kyle Bebak <kylebebak@gmail.com>
2016-11-05 21:39:07 -0600

    starting to flesh out helpers

--- pick-git/helpers.py
+++ pick-git/helpers.py
@@ -1,6 +1,21 @@
-class MySample:
-    def __init__(self):
-        pass
+import subprocess, os
 
-    def name(self):
-        return "my name"
+import pyperclip
+
+
+def repository_root():
+    return subprocess.check_output(('git', 'rev-parse', '--show-toplevel',)).strip()
+
+
+def cd_repository_root():
+    os.chdir(repository_root())
+
+
+def current_branch():
+    return subprocess.check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD',)).strip()
+
+
+def pick_branch():
+    branches = subprocess.Popen(('git', 'branch', '-a',), stdout=subprocess.PIPE)
+    branch = subprocess.check_output(('pick',), stdin=branches.stdout)
+    return branch.split()[-1]
